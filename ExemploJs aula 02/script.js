332e6c8848165456e6a7be40abfe01d9834f4977
neres <leonardo.neres2010@gmail.com>
2025-05-09 10:47:37 -0300

    Update script.js

--- ExemploJs aula 02/script.js
+++ ExemploJs aula 02/script.js
@@ -1,13 +1,10 @@
 let numero1 = document.querySelector("#numero1");
 let numero2 = document.querySelector("#numero2");
-let somar = document.querySelector("#somar");
 
-function somar() {
-    let num1 = parseFloat(numero1.value)
-    let num2 = parseFloat(numero2.value)
-    let somanums = num1 + num2
-    let resultado = document.getElementById('resultado')
-    resultado.innerText = `resultado: ${somanums}`
-
-
+function somarNumeros() {
+    let num1 = parseFloat(numero1.value);
+    let num2 = parseFloat(numero2.value);
+    let somanums = num1 + num2;
+    let resultado = document.getElementById('resultado');
+    resultado.innerText = `resultado: ${somanums}`;
 }
